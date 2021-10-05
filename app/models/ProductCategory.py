d7b0b1e82ccc9c7055dc9e6ce0888617cff5ab9e
Ming119 <ming2007119@gmail.com>
2021-10-05 10:27:35 -0700

    Add a new Category by staff

--- app/models/ProductCategory.py
+++ app/models/ProductCategory.py
@@ -6,7 +6,7 @@ class ProductCategory(db.Model):
 
     category_id = db.Column(db.Integer, primary_key=True);
 
-    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False);
+    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=True);
 
     name        = db.Column(db.String(64),  nullable=False);
     description = db.Column(db.String(255), nullable=True);
--- app/routes/productRoute.py
+++ app/routes/productRoute.py
@@ -1,13 +1,13 @@
 from flask import Blueprint;
 from controllers.productController import (
-    index, search, details, edit, create, delete
+    index, details, edit, createProduct, createCatedory, delete
 );
 
 bp = Blueprint('product', __name__, url_prefix='/product');
 
 bp.route('/', methods=['GET', 'POST'])(index);
-bp.route('/search', methods=['POST'])(search);
 bp.route('/<int:product_id>', methods=['GET'])(details);
 bp.route('/edit/<int:product_id>', methods=['POST'])(edit);
-bp.route('/create', methods=['POST'])(create);
+bp.route('/createProduct', methods=['POST'])(createProduct);
+# bp.route('/createCategory', methods=['POST'])(createCatedory);
 bp.route('/delete', methods=['POST'])(delete);
