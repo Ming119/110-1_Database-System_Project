49005e61278bdb1988ecdaed8d5b2c2636807ca2
NTUT_Starvian <79505152+HuangShengYao108590061@users.noreply.github.com>
2021-10-27 09:49:46 -0700

    Update app/controllers/manageUserController.py
    Co-authored-by: NTUT_Ming <57996391+Ming119@users.noreply.github.com>

--- app/controllers/manageUserController.py
+++ app/controllers/manageUserController.py
@@ -4,9 +4,10 @@ from util import db;
 # from app.models import User;
 from flask import session, render_template, redirect, url_for;
 
-def manageUsers(user_id):    
+def manageUsers(user_id):
     users = load_all_users()
     return render_template("manageUser.html", userlist=users)
+    
 def show_date(create_at):
     user_date = load_user_date()
     return render_template("manageUser.html", userdatelist=user_date)
