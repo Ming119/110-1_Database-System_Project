5a72156bb247f6679ec602989d7d921a63428d49
star <t108590061@ntut.org.tw>
2021-10-27 09:30:01 -0700

    added button to rows in table

--- app/controllers/manageUserController.py
+++ app/controllers/manageUserController.py
@@ -7,6 +7,4 @@ from flask import session, render_template, redirect, url_for;
 def manageUsers(user_id):    
     users = load_all_users()
     return render_template("manageUser.html", userlist=users)
-def show_date(create_at):
-    user_date = load_user_date()
-    return render_template("manageUser.html", userdatelist=user_date)
+
