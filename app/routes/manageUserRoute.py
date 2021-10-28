44f0b5362f61cf915f82bb002ef50f066760eb50
NTUT_Starvian <79505152+HuangShengYao108590061@users.noreply.github.com>
2021-10-28 14:32:49 +0800

    Removed ;

--- app/routes/manageUserRoute.py
+++ app/routes/manageUserRoute.py
@@ -1,6 +1,6 @@
-from flask import Blueprint;
-from controllers.manageUserController import manageUsers, show_date;
+from flask import Blueprint
+from controllers.manageUserController import manageUsers, show_date
 
-bp = Blueprint('manageUser', __name__, url_prefix='/manageUsers');
+bp = Blueprint('manageUser', __name__, url_prefix='/manageUsers')
 
-bp.route('/<int:user_id>', methods=['GET'])(manageUsers);
+bp.route('/<int:user_id>', methods=['GET'])(manageUsers)
