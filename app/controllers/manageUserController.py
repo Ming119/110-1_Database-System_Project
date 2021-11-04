2463e0336d389452edc96013a2c2586bdadba995
Ming119 <ming2007119@gmail.com>
2021-11-04 23:33:24 +0800

    update MMS page layout.

--- app/controllers/manageUserController.py
+++ app/controllers/manageUserController.py
@@ -4,8 +4,4 @@ from app.models import User
 
 def manageUsers(user_id):
     users = User.User.query.all()
-    return render_template("manageUser.html", userlist=users)
-
-def show_date(create_at):
-    user_date = User.User.load_user_date()
-    return render_template("manageUser.html", userdatelist=user_date)
+    return render_template("manageUser.html", users=users)
