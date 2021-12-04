b8f4b2b61fd7b0f374bb67d2e8e049cd64064b8b
Ming119 <ming2007119@gmail.com>
2021-12-04 16:58:07 +0800

    Create New User by Admin role

--- app/controllers/userController.py
+++ app/controllers/userController.py
@@ -15,23 +15,68 @@ def index():
         users = User.getAll()
         return render_template("manageUser.html", users=users)
 
+    return render_template("newUser.html", newUserForm=newUserForm)
+
+
+
 @login_required
-def createNewUser(form):
-    userCheck = User.getByUsername(form.username.data)
-    if userCheck and userCheck.confirm:
-        flash(f'This username ({form.username.data}) is already exist', 'warning')
-        return redirect(url_for('index.register'))
-
-    # check that the email is used and confirmed
-    userCheck = User.getByEmail(form.email.data)
-    if userCheck and userCheck.confirm:
-        flash(f'This email ({form.email.data}) address is already exist', 'warning')
-        return redirect(url_for('index.register'))
-
-    # TODO: add new user function
+def createUser(role):
+    if current_user.role != 'admin':
+        flash(f'You are not allowed to access.', 'danger')
+        return redirect(url_for('index.index'))
 
 
-    return render_template("manageUser.html", users=users, form=NewUserForm())
+
+    if role == 'staff':
+        newUserForm = NewStaffForm()
+
+    elif role == 'customer':
+        newUserForm = NewCustomerForm()
+
+
+
+    if request.method == 'POST' and newUserForm.validate_on_submit():
+        # check that the username is used
+        userCheck = User.getByUsername(newUserForm.username.data)
+        if userCheck:
+            flash(f'This username ({newUserForm.username.data}) is already exist', 'warning')
+            return redirect(url_for('user.index'))
+
+        # check that the email is used
+        userCheck = User.getByEmail(newUserForm.email.data)
+        if userCheck:
+            flash(f'This email ({newUserForm.email.data}) address is already exist', 'warning')
+            return redirect(url_for('user.index'))
+
+        elif newUserForm.role == 'staff':
+            staff = Staff.create(
+                        username   = newUserForm.username.data,
+                        email      = newUserForm.email.data,
+                        password   = newUserForm.password.data,
+                        first_name = newUserForm.first_name.data,
+                        last_name  = newUserForm.last_name.data,
+                    )
+            flash(f'Staff created successfully!', 'success')
+
+        elif newUserForm.role == 'customer':
+            customer = Customer.create(
+                            username   = newUserForm.username.data,
+                            email      = newUserForm.email.data,
+                            password   = newUserForm.password.data,
+                            first_name = newUserForm.first_name.data,
+                            last_name  = newUserForm.last_name.data,
+                            DOB        = newUserForm.DOB.data
+                        )
+            flash(f'Customer created successfully!', 'success')
+
+        else:
+            flash(f'Error occurred when creating new user', 'danger')
+
+        return redirect(url_for('user.index'))
+
+    return render_template("newUser.html", newUserForm=newUserForm)
+
+
 
 @login_required
 def profile(user_id):
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,5 +1,4 @@
 # from app.forms.AddToCardForm import AddToCardForm
-# from app.forms.CreateNewUserForm import CreateNewUserForm
 # from app.forms.EditProfileForm import EditProfileForm
 from app.forms.ForgotPasswordForm import ForgotPasswordForm
 from app.forms.LoginForm import LoginForm
@@ -8,3 +7,6 @@ from app.forms.NewProductForm import NewProductForm
 from app.forms.RegisterForm import RegisterForm
 from app.forms.ResetPasswordForm import ResetPasswordForm
 from app.forms.SearchForm import SearchForm
+from app.forms.NewUserForm import (
+    NewStaffForm, NewCustomerForm
+)
