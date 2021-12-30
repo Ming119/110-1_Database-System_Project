381f6899c879dce744854011644e87be6fa10f99
Ming119 <ming2007119@gmail.com>
2021-12-31 00:53:35 +0800

    DMS finsih functional front-end.

---
+++ app/forms/NewDiscountForm.py
@@ -0,0 +1,81 @@
+from flask_wtf import Form
+from wtforms import StringField, FloatField, SubmitField, validators, ValidationError
+from wtforms.fields.html5 import DateField, IntegerRangeField
+
+class NewDiscountForm(Form):
+    code = StringField('Code', validators=[
+        validators.DataRequired()
+    ])
+
+    name = StringField('Name', validators=[
+        validators.DataRequired()
+    ])
+
+    description = StringField('Description', validators=[
+        validators.DataRequired()
+    ])
+
+    start_at = DateField('Start At', validators=[
+        validators.DataRequired()
+    ])
+
+    end_at = DateField('End At', validators=[
+        validators.DataRequired()
+    ])
+
+    submit = SubmitField('Submit')
+
+    def initData(self, discount):
+        self.code.data        = discount.discount_code
+        self.name.data        = discount.name
+        self.description.data = discount.description
+        self.start_at.data    = discount.start_at
+        self.end_at.data      = discount.end_at
+
+
+
+class NewShippingDiscountForm(NewDiscountForm):
+    atLeastAmount = FloatField('Amount Above', validators=[
+        validators.InputRequired(),
+        validators.NumberRange(min=0)
+    ])
+
+    def __init__(self, *arg, **kwargs):
+        self.type = 'shipping'
+        super(NewShippingDiscountForm, self).__init__(*arg, **kwargs)
+
+    def initData(self, discount):
+        super().initData(discount)
+        self.atLeastAmount.data = discount.atLeastAmount
+
+
+
+class NewProductDiscountForm(NewDiscountForm):
+    discountPercentage = IntegerRangeField('Discount Rate')
+
+    def __init__(self, *arg, **kwargs):
+        self.type = 'product'
+        super(NewProductDiscountForm, self).__init__(*arg, **kwargs)
+
+    def initData(self, discount):
+        super().initData(discount)
+        self.discountPercentage.data = discount.discountPercentage
+
+
+
+class NewOrderDiscountForm(NewDiscountForm):
+    atLeastAmount = FloatField('Amount Above', validators=[
+        validators.InputRequired(),
+        validators.NumberRange(min=0)
+    ])
+
+    discountPercentage = IntegerRangeField('Discount Rate')
+
+    def __init__(self, *arg, **kwargs):
+        self.type = 'ording'
+        super(NewOrderDiscountForm, self).__init__(*arg, **kwargs)
+
+    def initData(self, discount):
+        super().initData(discount)
+        self.atLeastAmount.data      = discount.atLeastAmount
+        self.discountPercentage.data = discount.discountPercentage
--- app/models/Discount.py
+++ app/models/Discount.py
@@ -41,6 +41,10 @@ class Discount(db.Model):
         return Discount.query.filter_by(is_active=True).all()
 
     @staticmethod
+    def getByCode(code):
+        return Discount.query.filter_by(discount_code=code).first()
+
+    @staticmethod
     def delete(discount_code):
         try:
             db.session.delete(Discount.getByID(discount_code))
@@ -86,7 +90,7 @@ class ShippingDiscount(Discount):
     @staticmethod
     def create(discount_code, name, start_at, end_at, atLeastAmount, description=None, type='shipping'):
         try:
-            db.session.add(Discount(
+            db.session.add(ShippingDiscount(
                 discount_code = discount_code,
                 name          = name,
                 description   = description,
@@ -126,7 +130,7 @@ class ProductDiscount(Discount):
     @staticmethod
     def create(discount_code, name, start_at, end_at, discountPercentage, description=None, type='product'):
         try:
-            db.session.add(Discount(
+            db.session.add(ProductDiscount(
                 discount_code      = discount_code,
                 name               = name,
                 description        = description,
@@ -167,7 +171,7 @@ class OrderDiscount(Discount):
     @staticmethod
     def create(discount_code, name, start_at, end_at, discountPercentage, atLeastAmount, description=None, type='order'):
         try:
-            db.session.add(Discount(
+            db.session.add(OrderDiscount(
                 discount_code      = discount_code,
                 name               = name,
                 description        = description,
