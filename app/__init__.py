4b0db8a6e2bdee7c19d7e66ea1840f51f03488cb
Ming119 <ming2007119@gmail.com>
2022-01-04 09:43:15 +0800

    [WIP] address

--- app/__init__.py
+++ app/__init__.py
@@ -26,11 +26,12 @@ def create_app(test_config=None):
 
     # Register Route Blueprint
     from app.routes import (
-        indexRoute, userRoute, productRoute, discountRoute
+        indexRoute, userRoute, productRoute, discountRoute, orderRoute
     )
     app.register_blueprint(indexRoute.bp)
     app.register_blueprint(userRoute.bp)
     app.register_blueprint(productRoute.bp)
     app.register_blueprint(discountRoute.bp)
+    app.register_blueprint(orderRoute.bp)
 
     return app
