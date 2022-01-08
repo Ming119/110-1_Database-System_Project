45f0650aeadeefe4932268430ec3078ffcf0fa6e
NTUT_Starvian <79505152+HuangShengYao108590061@users.noreply.github.com>
2022-01-08 16:18:24 +0800

    [WIP] OMS front-end (#45)
    * [WIP] OMS backend
    
    * fix OMS backend
    
    * add filter
    
    * re-struct templates
    
    * update templates
    
    * fix filter function in OMS
    
    Co-authored-by: Ming119 <ming2007119@gmail.com>
    Co-authored-by: NTUT_Ming <57996391+Ming119@users.noreply.github.com>

--- app/controllers/orderController.py
+++ app/controllers/orderController.py
@@ -6,49 +6,101 @@ from flask_login import current_user, login_required
 
 
 @login_required
-def index():
-    if current_user.role != 'staff':
+def index(user_id):
+    if current_user.role != 'staff' and current_user.user_id != user_id:
         flash(f'You are not allowed to access.', 'danger')
         return redirect(url_for('index.index'))
 
-    order = Order.getAll()
+    searchForm = SearchForm()
+    if request.method == 'POST' and searchForm.validate_on_submit():
+        words = searchForm.search.data.split(' ')
 
+        orders_list = list()
+        for word in words:
+            orders_list.extend(Order.getAllContains(word))
+        orders = set(orders_list)
 
+    elif current_user.role == 'staff':
+        orders = Order.getAll()
+    else:
+        orders = Order.getByCustomerID(user_id)
 
-    return render_template('order/manageOrder.html', order=order)
-
-@login_required
-def filterIndex(status):
-    if current_user.role != 'staff':
-        flash(f'You are not allowed to access.', 'danger')
-        return redirect(url_for('index.index'))
-
-    order = Order.getByStatus()
+    if current_user.role == 'staff':
+        orderCount = {'all': Order.count(),
+                        'processing': Order.count(status=0),
+                        'delivering': Order.count(status=1),
+                        'delivered': Order.count(status=2),
+                    }
+    else:
+        orderCount = {'all': Order.count(user_id),
+                        'processing': Order.count(status=0, customer_id=user_id),
+                        'delivering': Order.count(status=1, customer_id=user_id),
+                        'delivered': Order.count(status=2, customer_id=user_id),
+                    }
 
     return render_template('order/manageOrder.html',
-                            order=order,
-                            filter=status
+                            searchForm = searchForm,
+                            orders     = orders,
+                            orderCount = orderCount,
+                            filter     = -1
                         )
 
 
 @login_required
-def details(order_id):
-
+def filterIndex(user_id, status):
     if current_user.role != 'staff':
         flash(f'You are not allowed to access.', 'danger')
         return redirect(url_for('index.index'))
 
+    searchForm = SearchForm()
+    if request.method == 'POST' and searchForm.validate_on_submit():
+        words = searchForm.search.data.split(' ')
+
+        orders_list = list()
+        for word in words:
+            orders_list.extend(Order.getAllContains(word))
+        orders = set(orders_list)
+
+    elif current_user.role == 'staff':
+        orders = Order.getByStatus(status)
+    else:
+        orders = Order.getByCustomerID(user_id, status)
+
+    if current_user.role == 'staff':
+        orderCount = {'all': Order.count(),
+                        'processing': Order.count(0),
+                        'delivering': Order.count(1),
+                        'delivered': Order.count(2),
+                    }
+    else:
+        orderCount = {'all': Order.count(user_id),
+                        'processing': Order.count(0, user_id),
+                        'delivering': Order.count(1, user_id),
+                        'delivered': Order.count(2, user_id),
+                    }
+
+    return render_template('order/manageOrder.html',
+                            searchForm = searchForm,
+                            orders     = orders,
+                            orderCount = orderCount,
+                            filter     = status
+                        )
+
+
+
+@login_required
+def details(order_id):
     order = Order.getByID(order_id)
+    if current_user.role != 'staff' and current_user.user_id != order.customer_id:
+        flash(f'You are not allowed to access.', 'danger')
+        return redirect(url_for('index.index'))
 
     orders = Order.getAll()
     form = NewProductForm(orders)
 
     form.initProductData(order)
-    return render_template('order/manageOrder.html', form=form, order=order)
 
-
-
-
+    return render_template('order/orderDetail.html', form=form, orders=orders)
     #return render_template('order/manageOrder.html', order=order) #order/orderDeteail.html
 
 
@@ -58,5 +110,7 @@ def update(order_id):
     if current_user.role != 'staff':
         flash(f'You are not allowed to access.', 'danger')
         return redirect(url_for('index.index'))
+
+    order = Order.getByID(order_id)
 
     return render_template('order/manageOrder.html')    #FIXME
--- app/routes/discountRoute.py
+++ app/routes/discountRoute.py
@@ -6,6 +6,6 @@ bp = Blueprint('discount', __name__, url_prefix='/discount')    # DMS path prefi
 bp.route('/', methods=['GET', 'POST'])(index)                           # Index page of DMS
 bp.route('/filter/<type>', methods=['GET', 'POST'])(filterIndex)                           # Index page of DMS
 bp.route('/create/<type>', methods=['GET', 'POST'])(create)            # Create
-bp.route('/<discount_code>', methods=['GET'])(details)          # Read
+# bp.route('/<discount_code>', methods=['GET'])(details)          # Read
 bp.route('/update/<discount_code>', methods=['GET', 'POST'])(update)    # Update
 bp.route('/delete/<discount_code>', methods=['GET'])(delete)    # Delete
